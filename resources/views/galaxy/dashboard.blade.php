9a10d6608e0df0a1babaf81d8d1f98d3d1cbdc5c
raphael-voyance <raphael-dev@outlook.fr>
2024-10-14 12:13:48 +0200

    save

--- resources/views/galaxy/dashboard.blade.php
+++ resources/views/galaxy/dashboard.blade.php
@@ -115,12 +115,15 @@
             <x-ui.card title="Tirage de Tarot">
             
                 <ul>
-                    @if ($draws->count() > 0)
-                        @foreach ($draws as $draw)
-                            <li><a href="{{ route('tarot.get-draw-cards', $draw->id) }}">{{ $draw->created_at->format('d/m/Y') }}</a></li>
-                        @endforeach
+                    @forelse ($draws as $draw)
+                    @php
+                        $drawDatas = json_decode($draw->draw);
+                    @endphp
+                        <li><a class="inline-flex items-center gap-2 px-1 pt-1 text-sm font-medium leading-5 text-gray-400 hover:text-gray-200 hover:border-gray-300 focus:outline-none focus:text-gray-200 focus:border-gray-300 transition duration-150 ease-in-out" href="{{ route('tarot.get-draw-cards', $draw->id) }}"><i class="fa-thin fa-thin fa-cards" aria-hidden="true"></i> {{ $drawDatas->name }} du {{ $draw->created_at->format('d/m/Y') }}</a></li>
                         <li><a href="{{ route('tarot.draw-cards-index') }}">Voir tous vos tirages enregistrés</a></li>
-                    @endif
+                    @empty
+                        <li>Vous n'avez pas encore de tirages enregistrés.</li>
+                    @endforelse
                 </ul>
                 <x-slot:actions>
                     <x-ui.link label="Faire un tirage de Tarot" href="{{ route('tarot.index') }}" />
