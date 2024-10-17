8516c39fdb02f9637d0bcaabd698b7cf5c220c54
raphael-voyance <raphael-dev@outlook.fr>
2024-10-17 12:21:57 +0200

    save

--- resources/js/helpers/utils.js
+++ resources/js/helpers/utils.js
@@ -98,8 +98,7 @@ function formatDateTime(dateTime) {
         month: 'long',
         day: 'numeric',
         hour: '2-digit',
-        minute: '2-digit',
-        second: '2-digit'
+        minute: '2-digit'
     };
     return new Date(dateTime).toLocaleDateString('fr-FR', options);
 }
