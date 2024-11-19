8960012267e804bfc245d3ace29909bc291c0dad
raphael-voyance <raphael-dev@outlook.fr>
2024-11-19 17:11:55 +0100

    save overlay

--- resources/js/loadingPage.js
+++ resources/js/loadingPage.js
@@ -1,21 +1,17 @@
 window.addEventListener('load', () => {
-    
     const overlay_element = document.getElementById('overlay_loadingpage');
 
-    if(overlay_element) {
-        let myTimeout = setTimeout(removeOverlay, 100);
-
-        function removeOverlay() {
-            overlay_element.classList.add('opacity-0')
-            overlay_element.classList.remove('opacity-100')
-            setTimeout(function() {
-                overlay_element.classList.add('hidden');
-            }, 200);
-        }
+    if (overlay_element) {
+        removeOverlay();
     }
 
-})
+    function removeOverlay() {
+        overlay_element.classList.add('opacity-0');
+        overlay_element.classList.remove('opacity-100');
 
-window.addEventListener('DOMContentLoaded', () => {
-    
-})
+        // Ajouter un événement de fin de transition au lieu d'utiliser un deuxième `setTimeout`
+        overlay_element.addEventListener('transitionend', () => {
+            overlay_element.classList.add('hidden');
+        }, { once: true }); // `once: true` permet de retirer automatiquement l'événement après qu'il a été déclenché
+    }
+});
